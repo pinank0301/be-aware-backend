0153e738693bb48c9f8670da0458b5fd1819cfd5
yashrajvrma <vermayashraj2005@gmail.com>
2025-12-18 10:15:09 +0530

    feat: add prisma

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,30 @@
 import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const app = express()
 const port = 8000
 
-app.use(express.json())
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(cors(
+    {
+        origin: "http://localhost:3000",
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE"], // Ensure needed methods are allowed
+        allowedHeaders: ["Content-Type", "Authorization"], // Allow necessary headers
+    }
+))
 
 app.get('/', (req, res) => {
     res.send("Hello world")
+})
+
+app.post("/url", (req, res) => {
+
+
 })
 
 app.listen(port, () => {
