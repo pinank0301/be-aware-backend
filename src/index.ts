4522a59f25b741ea1fa5c641385a62ef68181dfa
pinank0301 <pinanktrivedi694@gmail.com>
2026-01-09 21:13:10 +0530

    minor chage

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,9 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", 
+    "https://be-aware-frontend.vercel.app"
+  ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"], // Ensure needed methods are allowed
     allowedHeaders: ["Content-Type", "Authorization"], // Allow necessary headers
